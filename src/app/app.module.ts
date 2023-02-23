e1ae12f8b3ff473d24e4e472554964c9725df702
Géza Nagy <geza.nagy@attrecto.com>
2023-02-23 15:35:15 +0100

    Session 01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppComponent} from './app.component';
-import {HeaderModule} from './shared/modules/header/header.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { HeaderModule } from './shared/modules/header/header.module';
+import { HomeModule } from './shared/modules/home/home.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HeaderModule,
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HeaderModule, HomeModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
