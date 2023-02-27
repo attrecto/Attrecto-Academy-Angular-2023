fe34fe721352d7a10d073f3a3fef16b4456507a7
Bálint <bbalint0404@gmail.com>
2023-02-27 11:55:03 +0100

    :sparkles: 1.feladat

--- src/app/shared/modules/home/home/home.component.ts
+++ src/app/shared/modules/home/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,12 +7,22 @@ import {Component, Input} from '@angular/core';
 })
 export class HomeComponent {
   @Input() counter: number = 0;
+  errorMessage: string = "";
+  decreaseDisabled: boolean = false;
 
   increaseCounter() {
+    if (this.decreaseDisabled) {
+      this.errorMessage = "";
+      this.decreaseDisabled = false;
+    }
     this.counter++;
   }
 
   decreaseCounter() {
-    this.counter--;
+    if (this.counter > 0) this.counter--; //ezek
+    else {
+      this.errorMessage = "A számláló értéke nem merülhet 0 alá!";
+      this.decreaseDisabled = true;
+    }
   }
 }
