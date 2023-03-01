2bab3f3287c5ef3b6b59422cf070c16fbd750ca8
Bálint <bbalint0404@gmail.com>
2023-03-01 12:03:11 +0100

    :bug: counter validaton now works using the input field

--- src/app/shared/modules/home/home/home.component.ts
+++ src/app/shared/modules/home/home/home.component.ts
@@ -6,7 +6,18 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  @Input() counter: number = 0;
+  private _counter: number = 0;
+
+  @Input()
+  set counter(value: number) {
+    if (value < 0)
+      this._counter = 0;
+    else
+      this._counter = value;
+  }
+  get counter() {
+    return this._counter;
+  }
   errorMessage: string = "";
   decreaseDisabled: boolean = false;
 
@@ -19,7 +30,7 @@ export class HomeComponent {
   }
 
   decreaseCounter() {
-    if (this.counter > 0) this.counter--; //ezek
+    if (this.counter > 0) this.counter--;
     else {
       this.errorMessage = "A számláló értéke nem merülhet 0 alá!";
       this.decreaseDisabled = true;
