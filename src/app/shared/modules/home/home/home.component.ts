9cd702b1d2e772c49af6e759eb0fe55223e91d64
Bálint <bbalint0404@gmail.com>
2023-03-02 17:42:42 +0100

    :children_crossing: Alert message is nicer now

--- src/app/shared/modules/home/home/home.component.ts
+++ src/app/shared/modules/home/home/home.component.ts
@@ -18,22 +18,16 @@ export class HomeComponent {
   get counter() {
     return this._counter;
   }
-  errorMessage: string = "";
-  decreaseDisabled: boolean = false;
+  errorMessage: string = "A számláló értéke nem merülhet 0 alá!";
+  lessThan0: boolean = false;
 
   increaseCounter() {
-    if (this.decreaseDisabled) {
-      this.errorMessage = "";
-      this.decreaseDisabled = false;
-    }
+    if (this.lessThan0) this.lessThan0 = false;
     this.counter++;
   }
 
   decreaseCounter() {
     if (this.counter > 0) this.counter--;
-    else {
-      this.errorMessage = "A számláló értéke nem merülhet 0 alá!";
-      this.decreaseDisabled = true;
-    }
+    else this.lessThan0 = true;
   }
 }
