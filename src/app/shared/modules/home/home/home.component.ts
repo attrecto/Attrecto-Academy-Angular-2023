19fd9c05cf7486793a4b10c6ccd8028d8a931c8f
Kovacs Balint <balintkovacs1001@gmail.com>
2023-03-05 22:09:08 +0100

    first task

--- src/app/shared/modules/home/home/home.component.ts
+++ src/app/shared/modules/home/home/home.component.ts
@@ -6,13 +6,25 @@ import {Component, Input} from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  @Input() counter: number = 0;
-
+  counter: number = 0;
+  errorMsg: string = null;
+  @Input()
+  
   increaseCounter() {
     this.counter++;
+    this.errorMsg = null;
   }
 
   decreaseCounter() {
-    this.counter--;
+    if(this.counter>0){
+      this.counter--;
+      
+    }
+    else{
+       this.counter = 0;
+       //alert(this.errorMsg);
+       this.errorMsg = "A counter értéke nem lehet negatív";
+    }
   }
+
 }
