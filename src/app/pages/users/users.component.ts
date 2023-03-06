bb5c0aecd402e7a281336bd861b6368874f26e3f
Viktor Élő <viktor.elo@attrecto.com>
2023-03-06 10:27:48 +0100

    session 03

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from './classes/User';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+  users: User[];
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.userService.getUsers().subscribe({
+      next: (users) => {
+        this.users = users;
+      },
+    });
+  }
 }
