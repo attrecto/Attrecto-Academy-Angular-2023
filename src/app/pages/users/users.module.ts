061c24f99d7e445224cc00e0f23c6363248d10f0
Kovacs Balint <balintkovacs1001@gmail.com>
2023-03-19 21:04:06 +0100

    table component

--- src/app/pages/users/users.module.ts
+++ src/app/pages/users/users.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {UsersComponent} from "./users.component";
 import { UserCardComponent } from './components/user-card/user-card.component';
+import { UserTableComponent } from './components/user-table/user-table.component';
 
 
 
 @NgModule({
-  declarations: [UsersComponent, UserCardComponent],
+  declarations: [UsersComponent, UserCardComponent, UserTableComponent],
   imports: [CommonModule],
 })
 export class UsersModule {}
