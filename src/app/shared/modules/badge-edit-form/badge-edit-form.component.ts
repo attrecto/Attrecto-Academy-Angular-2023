94d6736c0d0246bdd26b83095ce406b11547f023
ggomba <ggomba88@gmail.com>
2023-03-25 11:40:07 +0100

    generate badge-edit-form

---
+++ src/app/shared/modules/badge-edit-form/badge-edit-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-badge-edit-form',
+  templateUrl: './badge-edit-form.component.html',
+  styleUrls: ['./badge-edit-form.component.scss']
+})
+export class BadgeEditFormComponent {
+
+}
---
+++ src/app/shared/modules/badge-edit-form/badge-edit-form.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BadgeEditFormComponent } from './badge-edit-form.component';
+
+
+
+@NgModule({
+  declarations: [
+    BadgeEditFormComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BadgeEditFormModule { }
