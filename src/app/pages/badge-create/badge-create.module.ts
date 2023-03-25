a94eb34617cbb15fbfe30fdcdff1134c0d290dce
ggomba <ggomba88@gmail.com>
2023-03-25 11:37:01 +0100

    badge-create creating

---
+++ src/app/pages/badge-create/badge-create.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BadgeCreateComponent } from './badge-create.component';
+
+
+
+@NgModule({
+  declarations: [
+    BadgeCreateComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BadgeCreateModule { }
