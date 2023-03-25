6db5f5fc39468a0a036aae3b45c575f8428ba275
ggomba <ggomba88@gmail.com>
2023-03-25 11:38:10 +0100

    badge-edit creating

---
+++ src/app/pages/badge-edit/badge-edit.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BadgeEditComponent } from './badge-edit.component';
+
+
+
+@NgModule({
+  declarations: [
+    BadgeEditComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BadgeEditModule { }
